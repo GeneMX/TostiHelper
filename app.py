b07ff10b64b79c1a194523c14631781d2484016b
GeneMX <gandhi.salazar@gmail.com>
2026-03-05 23:20:50 -0600

    Enhance user query handling for promotions
    Added handling for promotions in the user query response.

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import google.generativeai as genai
 import pandas as pd
 
 # Configuración visual
-st.set_page_config(page_title="Tostadas Siberia - Asesor", page_icon="🌮")
-st.title("🌮 Tostadas Tipo Siberia: Asistente de Pedidos")
+st.set_page_config(page_title="Tostadas Tipo Siberia", page_icon="🌮")
+st.title("🌮 TostiTellez: Asistente de Pedidos")
 
 # 1. Conexión a tus datos (Link del CSV de Google Sheets)
 SHEET_CSV = "https://docs.google.com/spreadsheets/d/1SWaypb3Fq1rR_S1van5P8OYcJhO9m_lxYBAoLRgDjCk/edit?usp=sharing"
@@ -21,7 +21,7 @@ genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
 model = genai.GenerativeModel('gemini-2.5-flash')
 
 # 3. Interfaz de usuario
-pregunta = st.text_input("¿Qué se te antoja hoy?", placeholder="Ej: ¿Qué trae la tostada?")
+pregunta = st.text_input("¿Qué se te antoja hoy?", placeholder="Ej: ¿Qué trae la tostada?,´¿Qué promociones tienen?, ¿Cual es el costo de envio? ")
 
 if pregunta:
     menu = obtener_menu()
@@ -31,6 +31,7 @@ if pregunta:
     Tu especialidad son las Tostadas tipo Siberia (descripción: {menu}).
     Sé amable, usa modismos locales en Nuevo Leon,Mexico si es adecuado y antoja al cliente.
     Si preguntan por el envío, diles que el costo es de 30 pesos.
+    Si preguntan por promociones, diles el menu buscando solamente los productos que sean tipo Promo.
     Al final de cada respuesta, invita a darle al botón de abajo para pedir por WhatsApp.
     """
     
@@ -40,6 +41,7 @@ if pregunta:
     # Botón de cierre de venta
 
     st.link_button("✅ ¡Hacer mi pedido ahora!", "https://wa.me/8130447383?text=Hola! Quiero hacer un pedido.")
+
 
 
 
